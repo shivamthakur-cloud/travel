58b6624ec1de724032f85d5a8ab2a1b4d7e06827
shiva kalyan <shivakalyan3243@gmail.com>
2020-01-07 22:28:37 +0530

    @shiva-kalyan created the models file as said

--- front_app/models.py
+++ front_app/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 
-class UserRole(models):
-    pass
+class UserRole(models.Model):
+    role_id = models.AutoField(primary_key=True)
+    role_name = models.CharField(max_length=255, default="", null=True, blank=True, unique=True)
+
+
+class RoleDetails(models.Model):
+    role_id = models.ForeignKey(UserRole, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, default="", null=True, blank=True)
+    email = models.EmailField(max_length=255, default="", unique=True, null=True, blank=True)
+    password = models.CharField(max_length=255, default="", null=True, blank=True)
+    mobile = models.BigIntegerField(max_length=255, default="", unique=True, null=True, blank=True)
+    address = models.TextField(max_length=255, unique=True, null=True, blank=True)
+    gender = models.CharField(max_length=255, blank=True, null=True, default="")
